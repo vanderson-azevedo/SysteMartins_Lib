175b76668198c03b4a32a0ce7899e53f1b0f1fcc
Vanderson Azevedo <dubinelson@gmail.com>
2026-02-28 17:52:57 -0300

    Ajuste de CheckVersion

--- server/server.lua
+++ server/server.lua
@@ -6,19 +6,96 @@ local function DetectFramework()
     end
     
     if GetResourceState('creative') == 'started' then
-        return 'CreativeV3'
-    elseif GetResourceState('vrp') == 'started' then
         return 'CreativeV5'
+    elseif GetResourceState('vrp') == 'started' then
+        return 'CreativeV3'
     elseif GetResourceState('qb-core') == 'started' then
         return 'Qcore'
     elseif GetResourceState('qbx-core') == 'started' then
         return 'Qbox'
     end
-    
+
     return nil
 end
 
 Framework.Name = DetectFramework()
+
+print("^6==========================================================^7")
+print("^4[SysteMartins]^7 Site: ^5https://systemartins.com.br^7")
+print("^4[SysteMartins]^7 Discord: ^5https://discord.systemartins.com.br^7")
+print("^6==========================================================^7")
+print('^4[SyteMartins_Lib]^7 Framework detectado: ^3' .. (Framework.Name or 'Nenhum') .. '^7')
+print("^6==========================================================^7")
+
+local remoteVersionUrl = "https://systemartins.com.br/Stm_Versao.json"
+local originalname = "SysteMartins_Lib"
+local scriptName = GetCurrentResourceName()
+local localVersion = GetResourceMetadata(scriptName, 'version', 0) or "0.0.0"
+
+if (scriptName ~= originalname) then
+    print("^6==========================================================^7")
+    print("^4[SysteMartins]^7 Site: ^5https://systemartins.com.br^7")
+    print("^4[SysteMartins]^7 Discord: ^5https://discord.systemartins.com.br^7")
+    print("^6==========================================================^7")
+    print("^4[SysteMartins]^1 O NOME DO SEU RESOURCE ^3"..scriptName.."^1 ESTA INCORRETO!^7")
+    print("^4[SysteMartins]^1 RENOMEIE O RESOURCE PARA ^2"..originalname.."^1 E REINICIE O SERVIDOR.^7")
+    print("^6==========================================================^7")
+    return;
+end
+
+function verificarVersao()
+    PerformHttpRequest(remoteVersionUrl, function(code, data)
+        if code == 200 and data then
+            local success, remoteData = pcall(json.decode, data)
+            if success and remoteData and remoteData[originalname] and remoteData[originalname].Versao then
+                local remoteVersion = remoteData[originalname].Versao
+
+                print("^6==========================================================^7")
+                print("^4[SysteMartins]^7 Versao Local: ^3v"..localVersion.." ^7| Versao Disponivel: ^2v"..remoteVersion)
+                print("^4[SysteMartins]^7 Site: ^5https://systemartins.com.br^7")
+                print("^4[SysteMartins]^7 Discord: ^5https://discord.systemartins.com.br^7")
+                print("^6==========================================================^7")
+
+            if compararVersao(localVersion, remoteVersion) then
+                print("^4[SysteMartins]^1 A VERSAO LOCAL DO ^3"..scriptName.."^1 ESTA DESATUALIZADA!^7")
+                print("^4[SysteMartins]^1 FACA JA O DOWNLOAD DA NOVA VERSAO PELO ^5[ https://github.com/vanderson-azevedo/SysteMartins_Lib ]^7")
+            else
+                print("^4[SysteMartins]^7 SUA VERSAO ^3"..scriptName.."^7 ESTA ^2ATUALIZADA^7, OBRIGADO PELA PREFRENCIA.^7")
+            end
+                print("^6==========================================================^7")
+            end
+        end
+    end)
+end
+
+-- Função para comparar versões (retorna true se versão remota é maior)
+function compararVersao(localVer, remoteVer)
+    local lv, rv = {}, {}
+
+    for num in string.gmatch(localVer, "[0-9]+") do
+        table.insert(lv, tonumber(num))
+    end
+    for num in string.gmatch(remoteVer, "[0-9]+") do
+        table.insert(rv, tonumber(num))
+    end
+
+    for i = 1, math.max(#lv, #rv) do
+        local l = lv[i] or 0
+        local r = rv[i] or 0
+        if r > l then
+            return true
+        elseif r < l then
+            return false
+        end
+    end
+    return false
+end
+
+-- Chamar a verificação ao iniciar o recurso
+CreateThread(function()
+    Wait(3000)
+    verificarVersao()
+end)
 
 function Framework.GetCore()
     if Framework.Name == 'CreativeV3' or Framework.Name == 'CreativeV5' then
@@ -133,7 +210,7 @@ function Framework.GiveItem(Passport, Item, Amount) -- Adicionar Item na mão.
     if Framework.Name == 'CreativeV5' then
         return vRP.GiveItem(Passport, Item, Amount)
     elseif Framework.Name == 'CreativeV3' then
-        return return vRP.generateItem(Passport, Item, Amount, true)
+        return vRP.generateItem(Passport, Item, Amount, true)
     elseif Framework.Name == 'Qcore' or Framework.Name == 'Qbox' then
         local Player = Framework.GetCore().Functions.GetPlayerByCitizenId(Passport)
         return Player and Player.Functions.AddItem(Item, Amount)
@@ -179,5 +256,3 @@ end
 function Framework.ServerNotify(Source, Type, Message, Time)
     TriggerClientEvent("Notify", Source, Type, Message, "Restaurante", Time)
 end
-
-print('^2[SyteMartins_Lib]^7 Framework detectado: ^3' .. (Framework.Name or 'Nenhum') .. '^7')
